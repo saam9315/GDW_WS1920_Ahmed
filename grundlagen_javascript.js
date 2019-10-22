5e846a1cee6bbba4eb3b6d535a1ad9f61543972b
saam9315 <saam9315@gmail.com>
2019-10-22 13:09:08 +0200

    Aufgabe 3

--- grundlagen_javascript.js
+++ grundlagen_javascript.js
@@ -9,6 +9,7 @@ console.log("Osama Ahmed");
 // Variablen Deklarieren, denen Werte zuweisen und die auf der Konsole ausgeben
 const maxBewert = 5;
 var anzahlBewert=0;
+var sumeBewert=0;
 var currBewert= 0;
 
 currBewert = 4;
@@ -26,5 +27,32 @@ console.log("Max Bewertung ist: "+maxBewert+ '\n'+
 			"Anzahl Bewertungen: "+anzahlBewert+ '\n'+
             "Aktuelle Bewertung: "+currBewert );
 
-//Aufgabe 3
+anzahlBewert =0;
+currBewert =0;            
+// Aufgabe 3
+// Bewertungen vom User einlesen und entsprechnde Werte ändern
 
+// Scanner einlegen
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Bitte geben Sie eine Bewertung ein ", function(answer){
+    if (answer > maxBewert || answer < 0){
+        console.error("INVALID RATING!");
+        return rl.close();
+    }
+    else {
+        anzahlBewert++;
+			sumeBewert +=1*answer;
+			currBewert= sumeBewert /anzahlBewert;
+			
+		
+			console.log("Max Bewertung ist: "+maxBewert+ '\n'+
+					"Anzahl Bewetungen: "+anzahlBewert+ '\n'+
+					"Aktuelle Bewertung: "+currBewert);
+        return rl.close();
+    }
+})
