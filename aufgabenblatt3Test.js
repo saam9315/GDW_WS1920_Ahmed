f520f56ad38d6d1148ae6be433155fb900c33340
saam9315 <saam9315@gmail.com>
2019-11-05 15:33:47 +0100

    Aufgabenblatt 3 Aufgabe2

---
+++ aufgabenblatt3Test.js
@@ -0,0 +1,4 @@
+const file = require('/Users/Sam 1/Desktop/GDW/GDW_WS1920_Ahmed_Kolesnikov_Heinlein/aufgabenblatt3.js');
+
+file.findAndDelete('/Users/Sam 1/Desktop/GDW/GDW_WS1920_Ahmed_Kolesnikov_Heinlein/cities.json',"Leipzig");
+
