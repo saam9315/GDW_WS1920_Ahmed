3bc01ea3c54c3dc5559f28ee7ccfdbb08b1510e9
saam9315 <saam9315@gmail.com>
2019-11-07 12:12:54 +0100

    Enthält jetzt nur Aufgabenblatt1

--- grundlagen_javascript.js
+++ grundlagen_javascript.js
@@ -13,7 +13,7 @@ var sumeBewert=0;
 var currBewert= 0;
 var lastBewert=0;
 
-/* comment start 
+
 
 currBewert = 4;
 anzahlBewert = 2;
@@ -85,7 +85,6 @@ var bewert = function (){
 };
 bewert();
 
-Comment end*/
 
 // Aufgabe 5 
 // Eine Funktion zur Berechnung 
@@ -97,111 +96,4 @@ var berechnen = function(bewertung){
 			lastBewert= angegBewert;
 
 }
-
-
-//Aufgabenblatt 2
-
-let games = ["bfv","COD","RS"];
-
-console.log(games.length);
-
-games.forEach(function(item,index, array){
-console.log(item,index);
-});
-
-games.push("Borderlands");
-
-
-games.forEach(function(item,index, array){
-    console.log(item,index);
-    });
-
-games.pop();
-
-games.forEach(function(item,index, array){
-    console.log(item,index);
-	});
-	
-let person = {
-	firstName: "Osama",
-	lastName: "Ahmed",
-	age: 23
-};
-
-console.log(person.firstName, person.lastName);
-
-
-function student (firstName, lastName, age){
-	this.firstName=firstName;
-	this.lastName=lastName;
-	this.age=age;
-};
-
-let osama = new student ("Osama", "Ahmed", 23);
-
-console.log(osama.firstName, osama.lastName, osama.age);
-
-const parseName = name =>{ 
-	let split = name.split(" ");
-	return {
-		firstName:split[0],
-		lastName:split[1]
-	};
-};
-
-
-let st = parseName("Osama Ahmed");
-
-console.log(st.firstName, st.lastName);
-
-//Aufgabe 1
-let bewertungInfo = ["Bewertung 1", anzahlBewert, lastBewert];
-
-console.log(bewertungInfo.length, bewertungInfo[bewertungInfo.length-1]);
-
-
-//Aufgabe 2, 3, 4
-
-function Ratings (name, anzahlBestimmungen, sum, aktuelleBewert){
-	this.name = name;
-	this.anzahlBestimmungen=anzahlBestimmungen;
-	this.sum = sum;
-	this.aktuelleBewert=0;
-	this.durchschnitt = () => {
-		return this.sum/this.anzahlBestimmungen;
-			 
-
-	};
-};
-
-
-let rating1 = new Ratings("App1",5,25,0);
-
-console.log(rating1.name, rating1.anzahlBestimmungen, rating1.sum, rating1.durchschnitt());
-
-
-//Aufgaben 5
-
-const hello = "hello";
-
-function world () {
-	const world = "world";
-
-	var satz = hello.concat(" ",world);
-
-	console.log(satz);
-};
-
-
-world();
-
-
-function two (){
-	
-	console.log( world.concat(" ",hello)); 
-};
-
- //two(); // error cause world is defined within function hello scope
-
-
 
